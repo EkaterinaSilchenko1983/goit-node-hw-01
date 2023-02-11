c2ad8c321e1cb4c23829e52aaa065e2835da5354
Ekaterina Silchenko <maksil.17@gmail.com>
2023-02-11 19:02:29 +0200

    add first commit with contacts

---
+++ contacts.js
@@ -0,0 +1,47 @@
+const fs = require("fs/promises");
+const { v4 } = require("uuid");
+
+const path = require("path");
+
+const contactsPath = path.join(__dirname, "db/contacts.json");
+
+const listContacts = async () => {
+  const data = await fs.readFile(contactsPath);
+  const contacts = JSON.parse(data);
+  return contacts;
+};
+
+const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  // console.log(contacts[0]);
+
+  const contact = contacts.find((item) => item.id === contactId);
+  // console.log(contact);
+
+  if (!contact) {
+    return null;
+  }
+  return contact;
+};
+
+const addContact = async ({ name, email, phone }) => {
+  const contacts = await listContacts();
+  const newContact = { name, email, phone, id: v4() };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts));
+  return newContact;
+};
+
+const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((item) => item.id === contactId);
+  console.log(index);
+  if (index === -1) {
+    return null;
+  }
+  const [removeContact] = products.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts));
+  return removeContact;
+};
+
+module.exports = { listContacts, getContactById, addContact, removeContact };
